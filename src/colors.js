1d40ec48c4fbe2be2b7f94ccf953fbf3a0ef3078
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-09-21 18:10:56 +0700

    feat: UI colors

--- src/colors.js
+++ src/colors.js
@@ -1,12 +1,16 @@
 export const manicColors = {
+  background: {
+    editor: '#202024',
+    sideBar: '#242428',
+    toolbar: '#2b2b2f',
+  },
   foreground: {
     editor: '#f8f8f2',
+    toolbar: '#ccc',
   },
   border: {
-    bright: '#f8f8f2',
-    soft: '#525252',
-    dim: '#a3a3a3',
-    dark: '#363636',
+    main: '#3d3d40',
+    lighter: '#424248',
   },
   token: {
     comment: '#7f848e',
