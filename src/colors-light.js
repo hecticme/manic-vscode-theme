13cd5e80dc7ffbe97f1293c9c602413edb614f58
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-10-02 21:37:37 +0700

    chore: create new file for light color theme

---
+++ src/colors-light.js
@@ -0,0 +1,33 @@
+export const manicLightColors = {
+  transparent: '#0000',
+  accent: '#ebebeb',
+  background: {
+    editor: '#202024',
+    sideBar: '#242428',
+    toolbar: '#2b2b2f',
+    input: '#303034',
+  },
+  foreground: {
+    editor: '#f8f8f2',
+    toolbar: '#cccccc',
+    contrast: '#202024',
+  },
+  border: {
+    main: '#3d3d40',
+    lighter: '#424248',
+  },
+  tokenPalette: {
+    black: '#7f848e',
+    green: '#4cc699',
+    brightGreen: '#c5ea8e',
+    pink: '#da86d5',
+    red: '#f44747',
+    brightRed: '#df6273',
+    yellow: '#f1c076',
+    brightYellow: '#f8dcab',
+    blue: '#a4c4f2',
+    magenta: '#aaa2ff',
+    teal: '#b6ffe4',
+    orange: '#ffa38b',
+  },
+}
