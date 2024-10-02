6354a73d5fba70f14276aaca9b924f34932d93f6
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-10-02 21:38:37 +0700

    feat: update logic to build light theme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import fsPromises from 'node:fs/promises'
 import { manicTheme } from './theme.js'
+import { manicLightTheme } from './theme-light.js'
 
 fsPromises.mkdir('./themes', { recursive: true })
-  .then(() => fsPromises.writeFile(
-    './themes/manic.json',
-    JSON.stringify(manicTheme, null, 2)
-  ))
+  .then(() => Promise.all([
+    fsPromises.writeFile(
+      './themes/manic.json',
+      JSON.stringify(manicTheme, null, 2)
+    ),
+    fsPromises.writeFile(
+      './themes/manic-light.json',
+      JSON.stringify(manicLightTheme, null, 2)
+    ),
+  ]))
